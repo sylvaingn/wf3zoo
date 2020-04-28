a0b9439f680297900419205c02fa9b3cc0b38ca2
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-04-28 10:08:51 +0200

    modif include

--- index.php
+++ index.php
@@ -18,11 +18,11 @@
 
 <body>
     
-    <?php include("navbar.php") ?>
+    <?php include("partials/navbar.php") ?>
 
     <main role="main">
 
-    <?php include("jumbotron.php") ?>
+    <?php include("partials/jumbotron.php"); ?>
 
         <div class="album py-5 bg-light">
             <div class="container">
@@ -198,7 +198,7 @@
 
     </main>
 
-    <?php include("footer.php")?>
+    <?php include("partials/footer.php")?>
 
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>

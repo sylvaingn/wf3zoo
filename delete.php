b37bfd98cd68938a17bcc283ca41e3d3ea2b087c
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-04-29 11:11:43 +0200

    suppression variable inutile

--- delete.php
+++ delete.php
@@ -7,7 +7,7 @@ $bdd = new PDO('mysql:host=localhost;dbname=wf3zoo;charset=utf8;port=8889', 'roo
 $request = "DELETE FROM animal where id =".$_GET["id"];
 var_dump($request);
 $response = $bdd->query($request);
-$animal = $response->fetch(PDO::FETCH_ASSOC);
+
 
 header('Location: index.php');
 

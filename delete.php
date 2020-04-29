526b1a38cc9ad815246db91b07d8a8851f06034a
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-04-29 11:43:43 +0200

    MAJ de la requette en prepare/execute

--- delete.php
+++ delete.php
@@ -4,9 +4,15 @@
 var_dump($_GET);
 
 $bdd = new PDO('mysql:host=localhost;dbname=wf3zoo;charset=utf8;port=8889', 'root', 'root');
-$request = "DELETE FROM animal where id =".$_GET["id"];
-var_dump($request);
-$response = $bdd->query($request);
+$request = "DELETE 
+            FROM animal 
+            where id = :id";
+//var_dump($request);
+$response = $bdd->prepare($request);
+
+$response->execute([
+    "id" => $_GET["id"],
+]);
 
 
 header('Location: index.php');

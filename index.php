767a02fd7914ed72a525fe2d947e6bd8e21cd771
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-04-29 10:29:27 +0200

    suppression bouton

--- index.php
+++ index.php
@@ -55,14 +55,13 @@ $animals = $response->fetchAll(PDO::FETCH_ASSOC);
                             <div class="card-body">
                                 <p class="card-text">
                                     <a href="show.php?id=<?= $animal["id"] ?>"> <?= $animal["nom"] ?> / <?= $animal["espece"] ?></a>
+                                    <br>
+                                    <a href="edit.php?id=<?= $animal["id"] ?>">Editer l'animal</a>
+                                    <br>
+                                    <a href="confirmDelete.php?id=<?= $animal["id"] ?>">Supprimer l'animal</a>
+
                                 </p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                                    </div>
-                                    <small class="text-muted">9 mins</small>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>

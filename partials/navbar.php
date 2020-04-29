56d988a0b9cea8c6f075ba6f5c8c9d049a11aa09
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-04-29 11:42:59 +0200

    ajout href vers index + ajout "ajouter animal"

--- partials/navbar.php
+++ partials/navbar.php
@@ -13,13 +13,14 @@
                             <li><a href="#" class="text-white">Like on Facebook</a></li>
                             <li><a href="#" class="text-white">Email me</a></li>
                         </ul>
+                        <h4><a href="add.php" class="text-white">Ajouter un animal</a></h4>
                     </div>
                 </div>
             </div>
         </div>
         <div class="navbar navbar-dark bg-dark shadow-sm">
             <div class="container d-flex justify-content-between">
-                <a href="#" class="navbar-brand d-flex align-items-center">
+                <a href="index.php" class="navbar-brand d-flex align-items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="mr-2" viewBox="0 0 24 24" focusable="false">
                         <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
                         <circle cx="12" cy="13" r="4" /></svg>
